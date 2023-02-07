7a3213d68c34543a0dd2b1142662b93c240f92c0
saurav <echo.saurav@gmail.com>
2023-02-07 12:37:38 +0600

    Tue Feb  7 12:37:38 +06 2023

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
 import './App.css';
+
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import Contact from "./components/contact/Contact";
+import Home from './components/home/Home';
+import Nav from './components/nav/Nav';
+import NotFound from "./components/notfound/NotFound";
+import Projects from "./components/projects/Projects";
+import { Container } from "react-bootstrap";
+
+// odrer matter, expend link should be bellow 
+//because this is basically a switch case
+export const navigation = [
+  { title: "Home", path: "/", element: <Home /> },
+  { title: "Projects", path: "/projects", element: <Projects /> },
+  { title: "Contact", path: "/contact", element: <Contact /> },
+]
+
+// Making it work for one page app with bootstrap, 
+// but my plan is to make it functional as apple.com.
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Router>
+        <Nav />
+        <Container  fluid="md">
+          <Routes>
+            {navigation.map((item, index) => (
+              <Route path={item.path} element={item.element} />
+            ))}
+            <Route path="*" element={<NotFound />} />
+
+          </Routes>
+
+
+          <Routes>
+            <Route path="/contact" element={<h1>On contact only</h1>} />
+          </Routes>
+        </Container>
+
+      </Router>
+
+
+
     </div>
   );
 }
